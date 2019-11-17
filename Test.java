72f1b7dfed8ab10b44b19728d29ff700a649c1cd
Alexander <45214837+Dualve@users.noreply.github.com>
2019-11-17 19:04:41 +0300

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,10 @@
+public class Test {
+	public static void main(String[] args){
+		System.out.println("          Full name:  Dubadzelau Alexander Vitalievich");
+		System.out.println("          Group num:  10701118");
+		System.out.println(" Primary occupation:  Student");
+		System.out.println("            Hobbies:  My life is history in thoughts, traveling in the soul,");
+		System.out.println("		      rock in the blood and programming in the heart ");
+		System.out.println("Main slogan or goal:  Everything will be okay in the end. If it is not okay, it is not the end.");
+	}
+}
